d2778f21ff46529fd3d7084b14bc00a32bf987ec
giuseppesciacca <sciaccagi@gmail.com>
2023-07-14 11:26:02 +0200

    add layout for sponsors/index + file scss

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -8,12 +8,12 @@
     <h4 class="text-center">A cosa serve la sponsorizzazione?</h4>
 
     <p class="pt-4"> La sponsorizzazione permetterà al tuo appartamento di:</p>
-    <ul class="pb-5">
+    <ul class="pb-5 list-unstyled">
         <li>
-            Farlo apparire in Homepage nella sezione “Appartamenti in Evidenza”;
+            <i class="fa-regular fa-hand-point-right" style="color: #ff7b7f;"></i> Farlo apparire in Homepage nella sezione “Appartamenti in Evidenza”;
         </li>
         <li>
-            Nella pagina di ricerca, verrà posizionato sempre prima di un appartamento non
+            <i class="fa-regular fa-hand-point-right" style="color: #ff7b7f;"></i> Nella pagina di ricerca, verrà posizionato sempre prima di un appartamento non
             sponsorizzato che soddisfa le stesse caratteristiche di ricerca.
         </li>
     </ul>
@@ -21,7 +21,7 @@
     <div class="row justify-content-center row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 ">
         @forelse ($sponsors as $sponsor)
         <div class="col">
-            <div class="card">
+            <div class="card card-sponsor">
                 <div class="card-header">
                     <h4 class="card-title text-center">{{$sponsor->name}}</h4>
                 </div>
@@ -32,25 +32,30 @@
                 </div>
 
                 <div class="card-footer">
-                    <p class="card-text">Prezzo: <strong>{{$sponsor->price}}</strong> €</p>
+                    <p class="card-text text-center">Prezzo: <strong class="price_sponsor text-center">{{$sponsor->price}} €</strong></p>
                 </div>
 
             </div>
-
         </div>
 
         @empty
-        <p>Sponsor non disponibili</p>
+        <p>Sponsor attualmente non disponibili</p>
         @endforelse
     </div>
 
     <p class="pt-3">
-        Per sponsorizzare un appartamento, vai su -> <strong>Appartamenti</strong> -> clicca sull'icona a forma di occhio per visualizzare l'appartamento che desideri sponsorizzare "<i class="fa-solid fa-eye"></i>" e clicca su -> <strong>Sponsorizzami</strong>. Da lì potrai scegliere uno dei tre tipi di sponsorizzazione sopra esposti.
+        Per sponsorizzare un appartamento, vai su -> <a class="text-decoration-none text-black fw-bold" href="{{ route('admin.apartments.index') }}">Appartamenti</a> -> clicca sull'icona a forma di occhio per visualizzare l'appartamento che desideri sponsorizzare "<i class="fa-solid fa-eye"></i>" e clicca su -> <strong class="text-black ">Sponsorizzami</strong>. Da lì potrai scegliere uno dei tre tipi di sponsorizzazione sopra esposti e andare nella sezione dedicata al pagamento.
     </p>
 
-    <p class="py-3">
-        Terminato il periodo di sponsorizzazione, l’appartamento tornerà ad essere
-        visualizzato normalmente. Potrai acquistare nuovamente il pacchetto promozionale quando vorrai.
+    <p class="pt-3">
+        <strong class="text-black">Puoi sponsorizzare più volte lo stesso appartamento</strong>, anche se l'attuale abbonamento non è ancora scaduto. L'abbonamento acquistato successivamente verrà inserito in coda.
+    </p>
+
+    <p class="py-3" id="conditions">
+        <em>
+            Terminato il periodo di sponsorizzazione, l’appartamento tornerà ad essere
+            visualizzato normalmente. Potrai acquistare nuovamente il pacchetto promozionale quando vorrai.
+        </em>
     </p>
 </div>
 @endsection
